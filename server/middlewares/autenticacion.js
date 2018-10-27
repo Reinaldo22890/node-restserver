8c68302af27d79ec500e7e52f783ac9310c91d6f
Reinaldo22890 <Reinaldo22890@gmail.com>
2018-10-27 16:56:32 -0400

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -23,6 +23,10 @@ let verificaToken = (req, res, next) => {
     });
 };
 
+//================================
+//VERIFICAR ROL DE ADMINISTRADOR
+//================================
+
 let verificaAdmin = (req, res, next) => {
 
     let usuario = req.usuario;
@@ -41,8 +45,32 @@ let verificaAdmin = (req, res, next) => {
 
 }
 
+//================================
+//VERIFICAR TOKEN PARA IMAGEN
+//================================
+
+let verificaTokenImg = (req, res, next) => {
+
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
+
 
 module.exports = {
     verificaToken,
-    verificaAdmin
+    verificaAdmin,
+    verificaTokenImg
 }
